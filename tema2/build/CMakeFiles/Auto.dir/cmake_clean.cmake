0d0bc49fc5474fb47382eb52e2305ff6dfd3e4b9
Cristinelunelu <92567479+Cristinelunelu@users.noreply.github.com>
2024-11-16 21:44:12 +0200

    Add files via upload

---
+++ tema2/build/CMakeFiles/Auto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Auto"
+  "Auto.pdb"
+  "CMakeFiles/Auto.dir/main.cpp.o"
+  "CMakeFiles/Auto.dir/masina.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Auto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tema2/main.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+#include<string>
+#include"masina.h"
+using namespace std;
+
+void display(Masina masina)
+{
+   masina.display();
+}
+
+int main()
+{
+    Masina Audi(1977, 2011, "A4", "Negru");
+    Masina Dacia(1363, 2007, "Logan", "Gri");
+   // Masina Mercedes=Audi;
+
+
+    Audi.display();
+    Dacia.display();
+    //Mercedes.display();
+
+
+   // Masina Renault=move(Dacia);
+
+   // Renault.display();
+    return 0;
+}
